115efc06d0c92b7867b3a3c61568f37d2ce4ef2e
Rodrione1 <alvarezrodrigofernando@gmail.com>
2022-05-21 13:06:20 -0300

    cargando arcivos

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+app.use(espress.static('public'));
+
+app.get("/",(req,res)=>{
+     res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+app.get("/babbage",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/babbage.html"))
+});
+app.get("/berners-lee",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/berners-lee.html"))
+});
+app.get("/clarke",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/clarke.html"))
+});
+app.get("/hopper",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/hopper.html"))
+});
+app.get("/lovelace",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/lovelace.html"))
+});
+
+app.get("/turing",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/turing.html"))
+});
+
+
+app.listen(3031, () => {
+ console.log ("escuchando en el puerto 3031");
+})
